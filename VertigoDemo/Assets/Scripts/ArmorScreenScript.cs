7e843fdaf95074f999cf7677e2fc1a769d994c4f
Muratcan Aşgün <masgun13@ku.edu.tr>
2017-10-03 03:53:54 +0300

    First Attempt
    First attempt on completing the interface

---
+++ VertigoDemo/Assets/Scripts/ArmorScreenScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmorScreenScript : MonoBehaviour
+{
+
+    
+    private GameObject armorScreen;
+    private GameObject weaponScreen;
+    private GameObject inventoryScreen;
+
+    void Start()
+    {
+        armorScreen = InventoryScript.getArmorScreen();
+        weaponScreen = InventoryScript.getWeaponScreen();
+        inventoryScreen = GameObject.Find("InventoryScreen");
+    }
+    
+    void Update()
+    {
+
+    }
+
+    public void goToArmorScreen()
+    {
+        InventoryScript.getArmorScreen().SetActive(true);
+        InventoryScript.getWeaponScreen().SetActive(false);
+        inventoryScreen.SetActive(false);
+    }
+    public void returnToInventory()
+    {
+        InventoryScript.getArmorScreen().SetActive(false);
+        InventoryScript.getWeaponScreen().SetActive(false);
+        inventoryScreen.SetActive(true);
+    }
+}
---
+++ VertigoDemo/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponScript : MonoBehaviour {
+
+   
+    //STATS
+    int attackPower;
+    int vitality;
+    int defense;
+    float attackSpeed;
+    int durability;
+    bool upgradeable;
+    bool disenchantable;
+    //
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update () {
+       
+
+    }
+   
+}
